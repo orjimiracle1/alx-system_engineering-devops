948125ef520096881db5b388c4ab4eabdd982848
orjimiracle1 <orjimiraclealx@gmail.com>
2024-03-01 05:20:29 -0800

    commit

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_hbtn(text)
-    regex = /hb(?:t){2,5}n/i
-    matches = text.scan(regex)
-    if !matches.empty?
-        puts matches.join('')
-    else
-        puts ""
-
-    end
-end
-
-text = ARGV[0]
-match_hbtn(text)
+puts ARGV[0].scan(/hbt{2,5}n/).join
