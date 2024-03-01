40df0883c61a85f6e7d64d3b7344cb3cf2d7aefe
orjimiracle1 <orjimiraclealx@gmail.com>
2024-03-01 05:11:00 -0800

    commit

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+def match_school(text)
+    regex = /School/
+
+    matches = text.scan(regex)
+    if !matches.empty?
+        puts matches.join('')
+    else
+        puts ""
+    end
+end
+
+text = ARGV[0]
+match_school(text)
